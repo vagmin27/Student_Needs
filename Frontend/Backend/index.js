9f1a15696228f7974adcbf876e89c0479e7f34f4
Vagmin Chiluka <vagmin2727@gmail.com>
2026-04-07 21:47:15 +0530

    Multiple Commits.

---
+++ Frontend/Backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+app.get("/", (req, res) => {
+    return res.json({message: "Server Started"})
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
